30a0514f2fdcdb4edf39e9884b6f53e1b340d355
nhathv <nhathv@aoi-sys.vn>
2015-11-20 10:25:53 +0700

    update template

--- djangobaseview/guestbook/urls.py
+++ djangobaseview/guestbook/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import patterns,url, include
-from django.views.generic import TemplateView
-from guestbook.views import IndexView, SignView, DeleteView, SendmailView, EditView
+from guestbook.views import IndexView, SignView, DeleteView, SendmailView, EditView, DojoGuestbook
 
 urlpatterns = patterns('',
 	url(r'^$', IndexView.as_view()),
@@ -9,5 +8,5 @@ urlpatterns = patterns('',
 	url(r'^delete/$', DeleteView.as_view()),
 	url(r'^sendmail/$', SendmailView.as_view()),
 	url(r'^api/', include('guestbook.api.urls')),
-	url(r'^dojo/$', TemplateView.as_view(template_name='guestbook/dojo-guestbook.html')),
+	url(r'^dojo/$', DojoGuestbook.as_view()),
 )
