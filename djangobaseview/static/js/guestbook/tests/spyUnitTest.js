24eb93b90143fb62218d7fdf8622e813d23b54b7
huuhuan <huanhh@aoi-sys.vn>
2015-12-02 13:34:53 +0700

    Create unit t

--- djangobaseview/static/js/guestbook/tests/spyUnitTest.js
+++ djangobaseview/static/js/guestbook/tests/spyUnitTest.js
@@ -1,32 +1,29 @@
-define([
-	"doh/runner",
-	"./sinon-1.17.2.js"
-], function (doh, sinon) {
-	//var callback = sinon.spy();
-	//var proxy = once(callback);
-	//
-	//proxy();
-	//
-	//assert(callback.called);
-	doh.register("MyTests", [
-		function assertTrueTest(){
-			doh.assertTrue(true);
-			doh.assertTrue(1);
-			doh.assertTrue(!false);
-		},
+define(["doh/runner", "../store/GreetingStore", "http://sinonjs.org/releases/sinon-1.17.2.js"], function(doh, GreetingStore){
+
+	doh.register("sinon.js", [
 		{
-			name: "thingerTest",
+			name: "server",
 			setUp: function(){
-				this.thingerToTest = new Thinger();
-				this.thingerToTest.doStuffToInit();
+				this.server = sinon.fakeServer.create();
+				this.server.respondWith("GET", "/api/guestbook/new_book/greeting/",
+					[200, {"Content-Type": "application/json"},
+						'[{ "guestbook_name": "new_book", "guestbook_mesage": "Unit test" }]']);
 			},
 			runTest: function(){
-				doh.assertEqual("blah", this.thingerToTest.blahProp);
-				doh.assertFalse(this.thingerToTest.falseProp);
-				// ...
+				var callback = sinon.spy();
+				var greetingStore = new GreetingStore("new_book", "Unit test", null);
+				greetingStore.addGuestbook().then(function () { callback = 1 });
+
+				this.server.respond();
+
+				doh.assertEqual(callback.callCount, 1);
+				//doh.assertTrue(callback.calledWith([{ "guestbook_name": "new_book", "guestbook_mesage": "Unit test" }]));
 			},
 			tearDown: function(){
+				this.server.restore();
 			}
-		}])
-	// ...
+		}
+		// ...
+	]);
+
 });
