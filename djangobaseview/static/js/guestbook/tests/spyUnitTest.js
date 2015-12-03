8864b2b2ee57e37cc19c6af3bea10acee6808134
huuhuan <huanhh@aoi-sys.vn>
2015-12-03 10:15:41 +0700

    Fix file unit test

--- djangobaseview/static/js/guestbook/tests/spyUnitTest.js
+++ djangobaseview/static/js/guestbook/tests/spyUnitTest.js
@@ -1,18 +1,22 @@
-define(["doh/runner", "../store/GreetingStore", "http://sinonjs.org/releases/sinon-1.17.2.js"], function(doh, GreetingStore){
+define([
+	"doh/runner",
+	"/static/js/sinon/sinon.js",
+	"../store/GreetingStore"
+], function(doh, sinon, GreetingStore){
 
-	doh.register("sinon.js", [
+	doh.register("GreetingStore", [
 		{
-			name: "server",
+			name: "Add guest book",
 			setUp: function(){
 				this.server = sinon.fakeServer.create();
-				this.server.respondWith("GET", "/api/guestbook/new_book/greeting/",
-					[200, {"Content-Type": "application/json"},
-						'[{ "guestbook_name": "new_book", "guestbook_mesage": "Unit test" }]']);
 			},
 			runTest: function(){
 				var callback = sinon.spy();
+				this.server.respondWith("GET", "/api/guestbook/new_book/greeting/",
+					[200, {"Content-Type": "application/json"},
+						'[{ "guestbook_name": "new_book", "guestbook_mesage": "Unit test" }]']);
 				var greetingStore = new GreetingStore("new_book", "Unit test", null);
-				greetingStore.addGuestbook().then(function () { callback = 1 });
+				greetingStore.addGuestbook().then(function () { callback = 1 }, function (status) { console.log(status); });
 
 				this.server.respond();
 
